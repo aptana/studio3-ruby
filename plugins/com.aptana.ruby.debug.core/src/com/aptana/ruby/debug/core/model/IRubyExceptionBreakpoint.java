497389a3d8010259ef6e02745cbd0ff006ab9b77
sgtcoolguy <cwilliams@aptana.com>
2010-12-06 13:01:06 -0500

    Copy over ruby/rails features and plugins

---
+++ plugins/com.aptana.ruby.debug.core/src/com/aptana/ruby/debug/core/model/IRubyExceptionBreakpoint.java
@@ -0,0 +1,7 @@
+package com.aptana.ruby.debug.core.model;
+
+import com.aptana.ruby.debug.core.IRubyBreakpoint;
+
+public interface IRubyExceptionBreakpoint extends IRubyBreakpoint
+{
+}
---
+++ plugins/com.aptana.ruby.debug.core/src/com/aptana/ruby/internal/debug/core/parsing/XmlStreamReaderException.java
@@ -0,0 +1,15 @@
+package com.aptana.ruby.internal.debug.core.parsing;
+
+public class XmlStreamReaderException extends Exception {
+
+	private static final long serialVersionUID = 2416062087800939179L;
+
+	public XmlStreamReaderException() {
+		super();
+	}
+
+	public XmlStreamReaderException(String s) {
+		super(s);
+	}
+
+}
