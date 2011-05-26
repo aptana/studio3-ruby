64b21fe548d2e2d4c8137484cbc14e9a390a15df
sgtcoolguy <cwilliams@aptana.com>
2011-05-26 16:13:52 -0700

    - Move ruby index utility methods to an abstract util class so we can re-use them.     - Do a better job determining if receiver of method call is a type or an instance to filter to singleton or instance proposals.     - Pass in project to type inferrer so we can get the correct indices for the project, and then use them to help further inferrencing.     - Handle inferring assignment/declaration nodes for variables by just calling infer recursively on the value node.     - Do a better job of inferring constant names. Search the indices to see if it's really a constant, then fall back to assuming it's the type name (which was the default and only behavior before).     - properly handle grabbing receiver node and inferring it's type and then suggesting methods on that type when after \\"::\\" (like we do after \\".\\")     - minor doc/task tweaks

--- plugins/com.aptana.ruby.core/src/com/aptana/ruby/core/codeassist/CompletionContext.java
+++ plugins/com.aptana.ruby.core/src/com/aptana/ruby/core/codeassist/CompletionContext.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
+import org.eclipse.core.resources.IProject;
 import org.jrubyparser.CompatVersion;
 import org.jrubyparser.StaticScope;
 import org.jrubyparser.ast.ClassNode;
@@ -50,8 +51,11 @@ public class CompletionContext
 	// Cache of the enclosing scope (typically type node or root) for instance/class vars
 	private Node enclosingScopeNode;
 
-	public CompletionContext(String src, int offset)
+	private IProject project;
+
+	public CompletionContext(IProject project, String src, int offset)
 	{
+		this.project = project;
 		this.src = src;
 		if (offset < 0)
 			offset = 0;
@@ -151,6 +155,8 @@ public class CompletionContext
 						if (partialPrefix == null)
 							partialPrefix = tmpPrefix.toString();
 						tmpPrefix.insert(0, ":");
+						offset = i + 1;
+						setOffset = true;
 						i--;
 					}
 				}
@@ -520,11 +526,12 @@ public class CompletionContext
 
 	public Collection<ITypeGuess> inferReceiver()
 	{
-		return new TypeInferrer().infer(getRootNode(), getReceiver());
+		return new TypeInferrer(project).infer(getRootNode(), getReceiver());
 	}
 
 	public boolean isNotParseable()
 	{
 		return getRootNode() == null;
 	}
+
 }
