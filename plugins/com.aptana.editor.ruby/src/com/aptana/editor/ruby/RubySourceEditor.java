b23a2e8b24214e797703ac58a8eba8e7e54d9b04
Chris Williams <cwilliams@appcelerator.com>
2011-11-18 11:42:52 -0500

    Set up RubyParseState for FileService

--- plugins/com.aptana.editor.ruby/src/com/aptana/editor/ruby/RubySourceEditor.java
+++ plugins/com.aptana.editor.ruby/src/com/aptana/editor/ruby/RubySourceEditor.java
@@ -25,11 +25,13 @@ import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.ui.internal.editors.text.EditorsPlugin;
 import org.eclipse.ui.texteditor.ChainedPreferenceStore;
 import org.eclipse.ui.texteditor.IDocumentProvider;
+import org.jrubyparser.CompatVersion;
 
 import com.aptana.core.logging.IdeLog;
 import com.aptana.editor.common.AbstractThemeableEditor;
 import com.aptana.editor.common.CommonEditorPlugin;
 import com.aptana.editor.common.outline.CommonOutlineItem;
+import com.aptana.editor.common.parsing.FileService;
 import com.aptana.editor.common.text.reconciler.IFoldingComputer;
 import com.aptana.editor.ruby.internal.text.RubyFoldingComputer;
 import com.aptana.editor.ruby.outline.RubyOutlineContentProvider;
@@ -41,6 +43,7 @@ import com.aptana.ruby.core.IRubyConstants;
 import com.aptana.ruby.core.IRubyElement;
 import com.aptana.ruby.core.IRubyMethod;
 import com.aptana.ruby.core.IRubyType;
+import com.aptana.ruby.core.RubyParseState;
 
 @SuppressWarnings("restriction")
 public class RubySourceEditor extends AbstractThemeableEditor
@@ -285,5 +288,16 @@ public class RubySourceEditor extends AbstractThemeableEditor
 	protected String getFileServiceContentTypeId()
 	{
 		return IRubyConstants.CONTENT_TYPE_RUBY;
+	}
+
+	@Override
+	protected FileService createFileService()
+	{
+		RubyParseState parseState = new RubyParseState();
+		parseState.setFilename(getEditorInput().getName());
+		parseState.setStartingLineNumber(1);
+		parseState.setVersion(CompatVersion.BOTH); // FIXME Determine the proper version based on the file here!
+		parseState.setWarnings(null); // ignore warnings
+		return new FileService(getFileServiceContentTypeId(), parseState);
 	}
 }
